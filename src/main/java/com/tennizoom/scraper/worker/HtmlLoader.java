57aa5da3d757ac74b2d21b2a428d7788e037e276
unknown <Tomek@Tomek-Komputer.(none)>
2014-02-22 17:02:34 +0100

    RF# Selenium added.

--- src/main/java/com/tennizoom/scraper/worker/HtmlLoader.java
+++ src/main/java/com/tennizoom/scraper/worker/HtmlLoader.java
@@ -1,6 +1,10 @@
 package com.tennizoom.scraper.worker;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.net.URL;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -10,21 +14,42 @@ import org.htmlcleaner.CleanerProperties;
 import org.htmlcleaner.DomSerializer;
 import org.htmlcleaner.HtmlCleaner;
 import org.htmlcleaner.TagNode;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.w3c.dom.Document;
 
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 import com.tennizoom.scraper.exception.HtmlLoaderException;
 
 
 public class HtmlLoader {
+	
+
+	WebDriver driver = new FirefoxDriver();
 	
 	private static Logger log = Logger.getLogger(HtmlLoader.class.getName());
 	
 	public Document loadCleanHtml(String urlString) throws HtmlLoaderException {
 		log.info("Loading html from: " + urlString);
 		try {
-			//WebView browser = new WebView();
-			//WebEngine webEngine = browser.getEngine();
-			//webEngine.load("http://mySite.com");
+			
+		    driver.get("http://nolags.pl/browse.php?u=c9FEDF%2Bmgrz1nz2NufwNI17beeuZn0E7XvRzBe48P%2BAa7T%2BnWyrgcUUBeJl4Vm1o7j%2FqOaW0vP99uAcNC0Xv7bD04z4zBlje%2B6dlcCgJdt8W6GnQzsqG8iLdqW%2F%2BwqCvSy%2FP4%2FRiM560iT3a%2BJwdMuBcImg03R3K8A%3D%3D&b=29");
+		    WebDriverWait wait = new WebDriverWait(driver, 3000);
+		    wait.until(ExpectedConditions.visibilityOfElementLocated((By.id("Email"))));
+
+		    WebElement nexPage = driver.findElement(By.xpath("/html/body/form[@id='form1']/div[3]/div[@id='MainDiv']/div[@id='MainContentHolderDiv']/div[@id='SectionHolderDiv']/span[@id='SectionCentreNav1']/span/div[3]/a[last()-1]"));
+		  //  WebElement userid = driver.findElement(By.id("Email"));
+		 //   WebElement password = driver.findElement(By.id("Passwd"));
+		  //  WebElement loginButton = driver.findElement(By.id("signIn")); 
+		  //  userid.sendKeys("landowskitomasz@gmail.com");
+		 //   password.sendKeys("j@n!ew!em");
+		    saveDocumentToFile(driver.getPageSource());
+		    nexPage.click();
 			
 			CleanerProperties props = new CleanerProperties();
 			props.setAllowHtmlInsideAttributes(true);
@@ -45,6 +70,24 @@ public class HtmlLoader {
 			throw new HtmlLoaderException("Unable to load html page.",e);
 		} catch (ParserConfigurationException e) {
 			throw new HtmlLoaderException("Unable to load html page.",e);
+		}
+	}
+	
+	private void saveDocumentToFile(String document) {
+		
+		try {
+			
+			File file = new File("selenium.html");
+			if(!file.exists()){
+				file.createNewFile();
+			}
+			
+			FileWriter fw = new FileWriter(file);
+			
+			fw.write(document);
+			fw.close();
+		} catch (IOException e) {
+
 		}
 	}
 
